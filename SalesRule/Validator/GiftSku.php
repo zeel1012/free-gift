8c29641aaf37989653c1c31c775aedcff29ecac7
Krzysztof Tomczyk <krzysztof.tomczyk@creativestyle.pl>
2024-05-09 11:27:07 +0000

    Feat: [MGS-5290] Validation of gift SKU

---
+++ SalesRule/Validator/GiftSku.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageSuite\FreeGift\SalesRule\Validator;
+
+class GiftSku
+{
+    protected \Magento\Framework\App\ResourceConnection $resourceConnection;
+
+    public function __construct(
+        \Magento\Framework\App\ResourceConnection $resourceConnection
+    ) {
+        $this->resourceConnection = $resourceConnection;
+    }
+
+    public function isValid(\Magento\Framework\DataObject $rule): bool
+    {
+        $skus = array_unique(explode(',', $rule->getGiftSkus()));
+
+        if (empty($skus)) {
+            return true;
+        }
+
+        $connection = $this->resourceConnection->getConnection();
+        $select = $connection->select()
+            ->from($this->resourceConnection->getTableName('catalog_product_entity'), 'COUNT(*)')
+            ->where('sku IN (?)', $skus);
+        $count = $connection->fetchOne($select);
+
+        return $count == count($skus);
+    }
+}
